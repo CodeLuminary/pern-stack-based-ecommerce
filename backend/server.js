bc04046835ce48c1daa3b96a6618ccb05437774e
CodeLuminary <victorijoni@yahoo.com>
2021-11-02 03:38:27 +0100

    Frontend & backend setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+app.use(cors());
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.listen();
+}
+else{
+    const port = process.env.PORT || 5000;
+    app.listen(port, ()=>console.log(`Server started on port ${port}`));
+}
