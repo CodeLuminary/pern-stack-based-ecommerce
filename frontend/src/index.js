c4a165478a647fc669775033b30319d730701185
CodeLuminary <victorijoni@yahoo.com>
2021-11-16 09:48:06 +0100

    Project updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,8 +11,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);  
+  document.getElementById('root') 
+);   
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
