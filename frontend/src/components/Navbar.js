04c4d71019eb40b3209d158404301afc25591761
CodeLuminary <victorijoni@yahoo.com>
2021-11-16 10:52:59 +0100

    Navbar added

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import "../css/navbar.css";
+import {Link} from "react-router-dom";
+const Navbar = ({click}) =>{
+
+    return (
+        <nav className="navbar">
+            <div className="hamburger__menu" onClick={click}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+      <div className="navbar__logo">
+        <h2>MERN Shopping Cart</h2>
+      </div>
+
+      <ul className="navbar__links">
+        <li>
+          <Link to="/cart" className="cart__link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart <span className="cartlogo__badge"></span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+      </ul>
+
+      
+    </nav>
+    );
+}
+
+export default Navbar;
