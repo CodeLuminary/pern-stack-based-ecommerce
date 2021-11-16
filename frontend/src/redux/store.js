b7b4d6cb4d678875d9a0ab8e3a3fa70f7a3fadd2
CodeLuminary <victorijoni@yahoo.com>
2021-11-16 20:30:31 +0100

    Reducer added

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counter";
+//import cartReducer from "./cart";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer
   }
 });
