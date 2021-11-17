0e327e4cb5fc075170f35ad5b7466537e7d1241e
CodeLuminary <victorijoni@yahoo.com>
2021-11-17 18:18:44 +0100

    CartReducer refactor

--- frontend/src/redux/reducers/cartReducer.js
+++ frontend/src/redux/reducers/cartReducer.js
@@ -4,7 +4,7 @@ let lastId = 0;
 
 export const cartSlice = createSlice({
     name: "cart",
-    initialState:{ value: []},
+    initialState:{ value: [],total = 0},
     reducers:{
         addItem: (state, action)=>{
             state.value = [
@@ -16,20 +16,24 @@ export const cartSlice = createSlice({
                     amount: action.payload.amount,
                     description: action.payload.description
                 }
-            ]
+            ];
+            state.total += 1;
         },
         removeItem: (state, action)=>{
+            state.total -= state.value[action.payload.id].quantity;
             state.value = state.value.filter( item => item.id !== action.payload.id)
         },
         decrementItemQuantity: (state, action)=>{
             state.value = state.value.map(item => item.id === action.payload.id ? {
                 ...item, quantity: --quantity
-            } : item )
+            } : item );
+            state.total -= 1;
         },
         incrementItemQuantity: (state, action)=>{
             state.value = state.value.map(item => item===action.payload.id ? {
                 ...item, quantity: ++quantity
-            } : item)
+            } : item);
+            state.total += 1;
         }
     }
 })
