51aa251c5ab4108dce0ab006081fbcc82da6fd49
CodeLuminary <victorijoni@yahoo.com>
2021-11-20 16:40:21 +0100

    store.js refactored

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./reducers/cartReducer";
+import productsReducer from "./reducers/productsReducer";
 
 export default configureStore({
   reducer: {
-    cart: cartReducer
+    cart: cartReducer,
+    products: productsReducer
   }
 });
