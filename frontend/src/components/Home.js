14ca6083a1812e70bcf3593889462272fa632578
CodeLuminary <victorijoni@yahoo.com>
2021-11-21 16:24:39 +0100

    Home.js updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,24 @@
 import "../css/home.css";
 import { useDispatch,useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect,useState} from "react";
 import api from '../Api'
 import { setProducts } from "../redux/reducers/productsReducer";
+import Product from "./Product";
 
 const Home = () =>{
-    //const products = useSelector((state)=>state.products);
+    const products = useSelector((state)=>state.products.value);
     const dispatch = useDispatch();
+    const [loadingState, setLoadingState] = useState("loading")
     
     const fetchProducts = async () =>{
         api.getApi('https://fakestoreapi.com/products')
         .then(response=>response.json())
         .then(result=>{
+            setLoadingState("ready")
             dispatch(setProducts(result))
+        })
+        .catch(err=>{
+            setLoadingState("error");
         })
     }
 
@@ -23,6 +29,19 @@ const Home = () =>{
 
     return(
         <div className="product-div">
+            {loadingState=="loading" ?
+                (<h1>Loading Products</h1>) :
+            loadingState=="error" ?
+                (<h1>Error loading products</h1>) :
+                (products.map((product)=>(
+                            <Product productProperties={product} />
+                        )
+                    )
+                )   
+            }
+            <div>
+
+            </div>
             <h1>Good</h1>
         </div>
     )
