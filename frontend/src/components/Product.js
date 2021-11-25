443ce2df1730536120112b8d9cf6edcecf78d7f3
CodeLuminary <victorijoni@yahoo.com>
2021-11-25 11:57:09 +0100

    Product.js refactored

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,15 +1,21 @@
 const Product=({productProperties})=>{
+    const getRating = () =>{
+        let spanArr = []
+        for(let i = 1; i <= productProperties.rating.rate.toFixed(0); i++){
+            spanArr.push('<span className="glyphicon glyphicon-star"></span>')
+        }
+    }
     return (
         <div className="product">
             <img src={productProperties.image} alt={`${productProperties.name} `}/>
             <span className="productName">{productProperties.title}</span><br/>
             <span className="productPrice">${productProperties.price}</span><br/>
             <p className="star">
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
-                <span className="glyphicon glyphicon-star"></span>
+                <span className={`glyphicon ${productProperties.rating.rate.toFixed() >= 1 ? 'glyphicon-star' : 'glyphicon-star-empty'}`}></span>
+                <span className={`glyphicon ${productProperties.rating.rate.toFixed() >= 2 ? 'glyphicon-star' : 'glyphicon-star-empty'}`}></span>
+                <span className={`glyphicon ${productProperties.rating.rate.toFixed() >= 3 ? 'glyphicon-star' : 'glyphicon-star-empty'}`}></span>
+                <span className={`glyphicon ${productProperties.rating.rate.toFixed() >= 4 ? 'glyphicon-star' : 'glyphicon-star-empty'}`}></span>
+                <span className={`glyphicon ${productProperties.rating.rate.toFixed() >= 5 ? 'glyphicon-star' : 'glyphicon-star-empty'}`}></span>
             </p>
         </div>
     );
