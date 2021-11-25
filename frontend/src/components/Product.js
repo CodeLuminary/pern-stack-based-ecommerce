85a4f293397cb26867a5d92b2f5cbb1ead7bb4d1
CodeLuminary <victorijoni@yahoo.com>
2021-11-25 11:37:48 +0100

    Product showcase updated

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,7 +3,14 @@ const Product=({productProperties})=>{
         <div className="product">
             <img src={productProperties.image} alt={`${productProperties.name} `}/>
             <span className="productName">{productProperties.title}</span><br/>
-            <span className="productPrice">${productProperties.price}</span>
+            <span className="productPrice">${productProperties.price}</span><br/>
+            <p className="star">
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+                <span className="glyphicon glyphicon-star"></span>
+            </p>
         </div>
     );
 }
