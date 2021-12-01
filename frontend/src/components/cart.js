a11d05ba0a0ef3450b4b00686e232b8cdaa4e517
CodeLuminary <victorijoni@yahoo.com>
2021-12-01 23:48:24 +0100

    cart updated

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -1,17 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import cartcss from "../css/cart.module.css";
 import Navbar from './Navbar.js';
+import Product from "./Product";
 import { decrementItemQuantity, incrementItemQuantity, removeItem } from "../redux/reducers/cartReducer";
 
 const Cart = () =>{
-    const cart = useSelector((state)=> state.cart)
+    const cart = useSelector((state)=> state.cart);
+
+    const removeItemFromCart = () =>{
+
+    }
 
     return (
         <div>
             <Navbar />
             <div className={cartcss.cart_panel}>
                 <p className={cartcss.item_total}>Cart ({`${cart.total} ${cart.total <= 1 ? "item": "items"}`})</p>
-                
+                {
+                    (cart.value.map((cart_item)=>{
+                        <div className={cartcss.cart_item_div} key={cart_item.id}>
+                            <Product productProperties={cart_item} css={cartcss} buttonObject={{click:removeItemFromCart,text:"Remove Item"}}/>
+                        </div>
+                    }))
+                }
             </div>
         </div>
     )
