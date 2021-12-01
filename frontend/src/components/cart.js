091db3d29fda1107b856ae6cb6626535212ae4f4
CodeLuminary <victorijoni@yahoo.com>
2021-12-01 18:57:38 +0100

    cart refactored

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -1,6 +1,19 @@
-const cart = () =>{
+import { useDispatch, useSelector } from "react-redux";
+import cartcss from "../css/cart.module.css";
+import Navbar from './Navbar.js';
+import { decrementItemQuantity, incrementItemQuantity, removeItem } from "../redux/reducers/cartReducer";
+
+const Cart = () =>{
+    const cart = useSelector((state)=> state.cart)
+
     return (
-        <div></div>
+        <div>
+            <Navbar />
+            <div className={cartcss.cart_panel}>
+                <p className={cartcss.item_total}>Cart ({`${cart.total} ${cart.total <= 1 ? "item": "items"}`})</p>
+                
+            </div>
+        </div>
     )
 }
-export default cart;
+export default Cart;
