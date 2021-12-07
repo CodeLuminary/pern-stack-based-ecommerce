1adb3a9a1f97254b9ac9aeb2f7d709163a0ff926
CodeLuminary <victorijoni@yahoo.com>
2021-12-07 13:35:33 +0100

    Project updated

--- frontend/src/components/admin/router.js
+++ frontend/src/components/admin/router.js
@@ -3,15 +3,23 @@ import Dashboard from './dashboard'
 import AddProduct from './addProducts';
 import ViewProducts from './viewProducts';
 import Sidebar from './sidebar';
+import admincss from '../../css/admin.module.css'
 const router = () =>{
     return (
         <div>
             <Sidebar />
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="products/add" element={<AddProduct />}/>
-                <Route path="products/view" element={<ViewProducts />} />
-            </Routes>
+            <div className={admincss.content}>
+                <div class="nav_top">
+                    <div><span id="uName" class="uName"><i class="fa fa-user"></i>&nbsp;John Doe</span><span className={admincss.logout} onclick="logout()" class="logou"><i class="fa fa-sign-out"></i>&nbsp;Log Out</span></div>        
+                </div>
+                <div className={admincss.app}>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="products/add" element={<AddProduct />}/>
+                    <Route path="products/view" element={<ViewProducts />} />
+                </Routes>
+                </div>
+            </div>   
         </div>
     )
 }
--- frontend/src/components/admin/sidebar.js
+++ frontend/src/components/admin/sidebar.js
@@ -5,8 +5,8 @@ const Sidebar = () =>{
     return (
         <div class={sidebar.nav}>
             <Link to="/" class={sidebar.nav__link} data-link><span></span><i class="fa fa-th-large"></i><span>Dashboard</span></Link>
-            <Link to="/posts" class={sidebar.nav__link} data-link><span></span><i class="fa fa-comment"></i><span>Posts</span></Link>
-            <Link to="/settings" class={sidebar.nav__link} data-link><span></span><i class="fa fa-cog"></i><span>Settings</span></Link>
+            <Link to="products/add" class={sidebar.nav__link} data-link><span></span><i class="fa fa-comment"></i><span>Add Product</span></Link>
+            <Link to="products/view" class={sidebar.nav__link} data-link><span></span><i class="fa fa-cog"></i><span>View Product</span></Link>
             <div class={sidebar.menuDet}><button id="clos">&#10094;</button><button id="opn">&#10095;</button></div>
         </div>
     )
