3142014731f3daee2a4e6a713eb244058fca0a12
CodeLuminary <victorijoni@yahoo.com>
2021-12-09 06:31:12 +0100

    AddProduct updated

--- frontend/src/components/admin/addProducts.js
+++ frontend/src/components/admin/addProducts.js
@@ -1,7 +1,17 @@
+import addPcss from "../../css/addProduct.module.css"
 const AddProduct = () =>{
     return (
         <div>
             <h1>Add Product</h1>
+            <span>Product Name<span className={addPcss.required}>*</span></span><br />
+            <input type="text" className={addPcss.input} placeholder="Enter product name" /><br />
+            <span>Product Amount<span className={addPcss.required}>*</span></span><br />
+            <input type="text" className={addPcss.input} placeholder="Enter product amount" /><br />
+            <span>Product Quantity<span className={addPcss.required}>*</span></span><br />
+            <input type="number" className={addPcss.input} placeholder="Enter product name" /><br />
+            <span>Product Description<span className={addPcss.required}>*</span></span><br />
+            <textarea className={addPcss.input}>
+            </textarea><br />
         </div>
     )
 }
