593775bce7b2a31518fce9dc8871831d3f5c9a01
CodeLuminary <victorijoni@yahoo.com>
2021-12-09 06:09:02 +0100

    Dashboard updated

--- frontend/src/components/admin/dashboard.js
+++ frontend/src/components/admin/dashboard.js
@@ -2,7 +2,19 @@
 const dashboard = () =>{
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>WELCOME</h1>
+            <p>This is a simple e-commerce admin panel. You can perform the following task here:</p>
+            <ul>
+                <li>
+                    Add Products
+                </li>
+                <li>
+                    View Added Products
+                </li>
+                <li>
+                    Delete Products
+                </li>
+            </ul>
         </div>
     )
 }
