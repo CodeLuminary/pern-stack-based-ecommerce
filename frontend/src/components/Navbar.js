ba42e9bc1c4d999cff7d15a32c2092b937ee6482
CodeLuminary <victorijoni@yahoo.com>
2022-03-24 09:52:05 +0100

    Project updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,10 +2,26 @@ import "../css/navbar.css";
 import {Link} from "react-router-dom";
 import {useSelector} from 'react-redux';
 
+import {AiOutlineSearch,AiOutlineHome} from "react-icons/ai";
+import {BsCart3,BsListUl} from "react-icons/bs";
+import {FaHome,FaUserCog} from "react-icons/fa";
+import headercss from "../css/header.module.css";
+
 const Navbar = ({click}) =>{
     const cart = useSelector((state)=> state.cart)
 
     return (
+      <>
+        <div className={`col-12 ${headercss.mobileFot}`}>
+                <Link to="/"><FaHome className={`${headercss.icon} ${headercss.active}`} /><br /><span className={`${headercss.mTxt}`}>Home</span></Link>
+                <Link to="/"><BsListUl className={headercss.icon} /><br /><span className={`${headercss.mTxt}`}>Categories</span></Link>
+                <Link className={headercss.cart} to="/cart"><BsCart3 style={{fontSize:25}}/>
+                        <span>{cart.total}</span>
+                        <br /><a className={`${headercss.mTxt}`}>Cart</a>
+                </Link>
+                <Link to="/"><FaUserCog className={headercss.icon} /><br /><span className={`${headercss.mTxt}`}>Account</span></Link>
+            </div>
+            
         <nav className="navbar">
             <div className="hamburger__menu" onClick={click}>
                 <div></div>
@@ -32,6 +48,7 @@ const Navbar = ({click}) =>{
 
       
     </nav>
+    </>
     );
 }
 
